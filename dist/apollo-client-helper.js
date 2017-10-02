de14e05a4f7c8fb4540f070d5061d08da8132797
nmondon <nmondon@lefigaro.fr>
2017-10-02 16:28:26 +0200

    build (#1)

--- dist/apollo-client-helper.js
+++ dist/apollo-client-helper.js
@@ -1,22 +1,32 @@
 'use strict';
 
-var apolloClient = require('apollo-client');
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var ApolloClient = _interopDefault(require('apollo-client'));
+var HttpLink = _interopDefault(require('apollo-link-http'));
+var InMemoryCache = _interopDefault(require('apollo-cache-inmemory'));
 
 var ApolloHelper = function ApolloHelper(uri) {
-  var networkInterface = apolloClient.createNetworkInterface({ uri: uri });
-  this.client = new apolloClient.ApolloClient({ networkInterface: networkInterface });
+    this.client = new ApolloClient({
+        link: new HttpLink({ uri: uri }),
+        cache: InMemoryCache()
+    });
 };
 
 ApolloHelper.prototype.getMutation = function getMutation (mutation, variables) {
-    if ( variables === void 0 ) variables = {};
+        if ( variables === void 0 ) variables = {};
 
-  return this.client.mutate({ mutation: mutation, variables: variables }).then(function (result) { return result.data; });
+    return this.client
+        .mutate({ mutation: mutation, variables: variables })
+        .then(function (result) { return result.data; });
 };
 
 ApolloHelper.prototype.getQuery = function getQuery (query, variables) {
-    if ( variables === void 0 ) variables = {};
+        if ( variables === void 0 ) variables = {};
 
-  return this.client.query({ query: query, variables: variables }).then(function (result) { return result.data; });
+    return this.client
+        .query({ query: query, variables: variables })
+        .then(function (result) { return result.data; });
 };
 
 module.exports = ApolloHelper;
